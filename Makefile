23139b21fd71169c94dd1e4f515bb02b3eebdd1f
Tomáš Hobza <hobza.tomas@gmail.com>
2025-03-12 16:07:11 +0100

    add: motor speed publisher

--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@ build:
 	colcon build
 
 run: build
+	$(source /opt/ros/humble/setup.bash)
 	$(source install/setup.bash)
-	export ROS_DOMAIN_ID=1
+	$(export ROS_DOMAIN_ID=1)
+	echo $(ROS_DOMAIN_ID)
 	ros2 run MyProject my_program
 
 .PHONY: build run
--- src/robots/src/main.cpp
+++ src/robots/src/main.cpp
@@ -1,5 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
-#include "RosExampleClass.h"
+// #include "RosExampleClass.h"
+#include "MotorNode.h"
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
@@ -9,15 +10,16 @@ int main(int argc, char* argv[]) {
 
     // Create multiple nodes
     auto node1 = std::make_shared<rclcpp::Node>("node1");
-    auto node2 = std::make_shared<rclcpp::Node>("node2");
+    // auto node2 = std::make_shared<rclcpp::Node>("node2");
 
     // Create instances of RosExampleClass using the existing nodes
-    auto example_class1 = std::make_shared<RosExampleClass>(node1, "topic1", 1.0);
-    auto example_class2 = std::make_shared<RosExampleClass>(node2, "topic2", 2.0);
+    // auto example_class1 = std::make_shared<RosExampleClass>(node1, "topic1", 1.0);
+    // auto example_class2 = std::make_shared<RosExampleClass>(node2, "topic2", 2.0);
+    auto example_class2 = std::make_shared<MotorNode>(node1);
 
     // Add nodes to the executor
     executor->add_node(node1);
-    executor->add_node(node2);
+    // executor->add_node(node2);
 
     // Run the executor (handles callbacks for both nodes)
     executor->spin();
