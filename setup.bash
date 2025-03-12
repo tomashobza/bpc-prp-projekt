788c6180f5436419cdf166845bc27241805a3c51
jacobs <daniel.jacobs@outlook.cz>
2025-03-12 15:29:09 +0100

    add: setup.bash

---
+++ setup.bash
@@ -0,0 +1,11 @@
+colcon build
+
+source install/setup.bash
+
+source /opt/ros/humble/setup.bash
+
+export ROS_DOMAIN_ID=1
+
+source ~/.bashrc
+
+echo $ROS_DOMAIN_ID
---
+++ src/robots/src/io_node.cpp
@@ -0,0 +1,15 @@
+#include "../include/robots/io_node.hpp"
+
+namespace {
+    IoNode::IoNode() {
+       // ...
+    }
+
+    IoNode::get_button_pressed() const {
+       // ...
+    }
+
+    // ...
+}   
+
+> ```
